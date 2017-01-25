9785d2aee1a3dce2c7c3447a3f697f2ee2f40c30
volkovasystems <richeve.bebedor@gmail.com>
2017-01-25 13:20:14 +0800

    Updated dependency.

---
+++ .install.sh
@@ -0,0 +1,33 @@
+yarn add \
+babel-runtime \
+asea \
+budge \
+harden \
+impel \
+kein \
+letgo \
+pringe \
+protype \
+truly \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+asea \
+budge \
+harden \
+impel \
+kein \
+letgo \
+pringe \
+protype \
+truly \
+zelf \
+--save
