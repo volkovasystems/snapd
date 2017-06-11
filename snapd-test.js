dbcfb3c8f45bff2726202aecc70a602100e7bba8
volkovasystems <richeve.bebedor@gmail.com>
2017-06-12 04:26:13 +0800

    Updated dependencies.

--- snapd-test.js
+++ snapd-test.js
@@ -1,4 +1,12 @@
 
 const snapd = require( "./snapd.js" );
 
-snapd( function test( ){ console.log( "hello", arguments ); return "yehey"; }, 1000, 1, 2, 3 )( function hello( ){ console.log( "called!", arguments ); } );
+snapd( function test( ){
+	console.log( "hello", arguments );
+
+	return "yehey";
+}, 1000, 1, 2, 3 )
+
+( function hello( ){
+	console.log( "called!", arguments );
+} );
